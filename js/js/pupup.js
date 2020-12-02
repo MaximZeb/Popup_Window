e9d693c908a18d3ef8afd81305df4eced9dd1917
Maxim Zebzeev <maksimzeb@yandex.ru>
2020-12-02 14:28:42 +0500

    Added java variable

--- js/js/pupup.js
+++ js/js/pupup.js
@@ -1,1 +1,4 @@
 "use strict"
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelecotorAll(".lock-padding");
